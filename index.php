eab1bd2bb7f3cf06f475b9361ebaa637d785db17
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-11-04 23:08:51 +0200

    Updated-3.

--- index.php
+++ index.php
@@ -5,57 +5,51 @@
     <title>Test</title>
 </head>
 <body>
-
 <?php
-echo
-'
-            <form method="post" action="index.php">
-                <p>Введите URL:<input name="URL"></p>
-                <p>Введите Description:<input name="Description"></p>
-                <p>Введите Cost:<input name="Cost"></p>
-                <p>Введите Date:<input name="Date"></p>
-                <p>Введите Adress:<input name="Adress"></p>
-                <p>Введите isTop:<input name="isTop" max="1" min="0"></p>
-                <input type="submit">
-            </form>
-        ';
-if
-(isset
-    ($_POST['URL']) &&
-    ($_POST['Description']) &&
-    ($_POST['Cost']) &&
-    ($_POST['Date']) &&
-    ($_POST['Adress']) &&
-    ($_POST['isTop'])
-)
-{
-    echo
-        '
-            <table style="background: ghostwhite" align="center" width="31%">
-                <tr>
-                    <td rowspan="5"><img align="right" width="150px" height="150px" alt="OLX_Img_1" src="' . $_POST['URL'] . '"</td>
-                    <td style="background: lightgoldenrodyellow">' . $_POST['Description'] . '</td>
-                </tr>
-                <tr>
-                    <td style="background: lightgoldenrodyellow">' . $_POST['Cost'] . '</td>
-                    </tr>
-                <tr>
-                    <td style="background: lightgoldenrodyellow">' . $_POST['Date'] . '</td>
-                </tr>
-                <tr>
-                    <td style="background: lightgoldenrodyellow">' . $_POST['Adress'] . '</td>
-                </tr>
-                <tr>
-                    <td style="background: lightgoldenrodyellow">' . $_POST['isTop'] . '</td>
-                </tr>
-            </table>
-        ';
-}
+//if (isset($_POST['URL']) && ($_POST['Description']) && ($_POST['Cost']) && ($_POST['Date']) && ($_POST['Adress']) && ($_POST['isTop']))
+//{
+//    echo
+//        '
+//            <table style="background: ghostwhite" align="center" width="31%">
+//                <tr>
+//                    <td rowspan="5"><img align="right" width="150px" height="150px" alt="OLX_Img_1" src="' . $_POST['URL'] . '"</td>
+//                    <td style="background: lightgoldenrodyellow">' . $_POST['Description'] . '</td>
+//                </tr>
+//                <tr>
+//                    <td style="background: lightgoldenrodyellow">' . $_POST['Cost'] . '</td>
+//                    </tr>
+//                <tr>
+//                    <td style="background: lightgoldenrodyellow">' . $_POST['Date'] . '</td>
+//                </tr>
+//                <tr>
+//                    <td style="background: lightgoldenrodyellow">' . $_POST['Adress'] . '</td>
+//                </tr>
+//                <tr>
+//                    <td style="background: lightgoldenrodyellow">' . $_POST['isTop'] . '</td>
+//                </tr>
+//            </table>
+//        ';
+//}
+//else
+//{
+//    echo
+//    '
+//            <form method="post" action="index.php">
+//                <p>Введите URL:<input name="URL"></p>
+//                <p>Введите Description:<input name="Description"></p>
+//                <p>Введите Cost:<input name="Cost"></p>
+//                <p>Введите Date:<input name="Date"></p>
+//                <p>Введите Adress:<input name="Adress"></p>
+//                <p>Введите isTop:<input name="isTop" max="1" min="0"></p>
+//                <input type="submit">
+//            </form>
+//        ';
+//}
+//
 ?>
-
 <form method="get" action="index.php">
     <?php
-    $asoc=
+    $ads_array =
         [
             0 => [
                 "URL" => "https://ireland.apollo.olxcdn.com/v1/files/u7rxfuvhyucm2-UA/image;s=644x461",
@@ -142,11 +136,11 @@ if
                 "Adress" => "Николаев, Николаевская область, Центральный",
                 "isTop" => true],
             12 => [
-                "URL" => "https://ireland.apollo.olxcdn.com/v1/files/z8l7n1h0n5pp3-UA/image;s=1000x700",
-                "Description" => "• Запаковані Нові • iPhone X 64 Айфон Х 7 8+ plus Xr Xs max 32 128 256",
-                "Cost" => 14199,
-                "Date" => "15:45, 19 октября 2020",
-                "Adress" => "Киев, Киевская область, Днепровский",
+                "URL" => "https://ireland.apollo.olxcdn.com/v1/files/05eq552mcbic3-UA/image;s=1000x700",
+                "Description" => "Новые. iPhone 6 16/64gb Sp. Gray/Gold/Silver айфон new 5 7 Х +",
+                "Cost" => 3999,
+                "Date" => "17:35, 20 октября 2020",
+                "Adress" => "Николаев, Николаевская область, Центральный",
                 "isTop" => true],
             13 => [
                 "URL" => "https://ireland.apollo.olxcdn.com/v1/files/05eq552mcbic3-UA/image;s=1000x700",
@@ -169,12 +163,19 @@ if
                 "Date" => "17:35, 20 октября 2020",
                 "Adress" => "Николаев, Николаевская область, Центральный",
                 "isTop" => true],
+            15 => [
+                "URL" => "https://ireland.apollo.olxcdn.com/v1/files/05eq552mcbic3-UA/image;s=1000x700",
+                "Description" => "Новые. iPhone 6 16/64gb Sp. Gray/Gold/Silver айфон new 5 7 Х +",
+                "Cost" => 3999,
+                "Date" => "17:35, 20 октября 2020",
+                "Adress" => "Николаев, Николаевская область, Центральный",
+                "isTop" => true],
             16 => [
-                "URL" => "https://ireland.apollo.olxcdn.com/v1/files/u7rxfuvhyucm2-UA/image;s=644x461",
-                "Description" => "Защитное стекло на айфон х,11Pro/11Pro Max,6,7,8+,Хr,5,5s,se",
-                "Cost" => 69,
-                "Date" => "12:49, 20 октября 2020",
-                "Adress" => "Ирпень, Киевская область",
+                "URL" => "https://ireland.apollo.olxcdn.com/v1/files/05eq552mcbic3-UA/image;s=1000x700",
+                "Description" => "Новые. iPhone 6 16/64gb Sp. Gray/Gold/Silver айфон new 5 7 Х +",
+                "Cost" => 3999,
+                "Date" => "17:35, 20 октября 2020",
+                "Adress" => "Николаев, Николаевская область, Центральный",
                 "isTop" => true],
             17 => [
                 "URL" => "https://ireland.apollo.olxcdn.com/v1/files/05eq552mcbic3-UA/image;s=1000x700",
@@ -182,59 +183,51 @@ if
                 "Cost" => 3999,
                 "Date" => "17:35, 20 октября 2020",
                 "Adress" => "Николаев, Николаевская область, Центральный",
-                "isTop" => true],
-            18 => [
-                "URL" => "https://ireland.apollo.olxcdn.com/v1/files/z8l7n1h0n5pp3-UA/image;s=1000x700",
-                "Description" => "• Запаковані Нові • iPhone X 64 Айфон Х 7 8+ plus Xr Xs max 32 128 256",
-                "Cost" => 14199,
-                "Date" => "15:45, 19 октября 2020",
-                "Adress" => "Киев, Киевская область, Днепровский",
-                "isTop" => true],
-            19 => [
-                "URL" => "https://ireland.apollo.olxcdn.com/v1/files/05eq552mcbic3-UA/image;s=1000x700",
-                "Description" => "Новые. iPhone 6 16/64gb Sp. Gray/Gold/Silver айфон new 5 7 Х +",
-                "Cost" => 3999,
-                "Date" => "17:35, 20 октября 2020",
-                "Adress" => "Николаев, Николаевская область, Центральный",
-                "isTop" => true],
+                "isTop" => true]
         ];
 
-    function loadNext($indpg,$asocs)
+    // ads_number - количество объявлений на страницу
+    // page_number - перечень страниц в пагинации - $_GET['Page']
+    // ads - ссылка на массив объявлений - $ads_array
+    // A3 - ?
+    // A4 - ?
+    // A5 - ?
+
+    function loadNext($page_number, $ads)
     {
-        $numberpage = 5;
-        if($indpg*$numberpage>=count($asocs)+$numberpage)
+        $ads_number = 5;
+        if($page_number * $ads_number >= count($ads) + $ads_number)
         {
             die("<div id='error'>Ошибка</div>");
         }
         else
         {
-            $tmpstrt = $indpg-1;
-            if($tmpstrt>=0)
+            $A3 = $page_number - 1;
+            if($A3 >= 0)
             {
-                $tmpfin=$indpg;
-                if ($tmpfin * $numberpage > count($asocs))
+                $A4 = $page_number;
+                if ($A4 * $ads_number > count($ads))
                 {
-                    $tmpfin = count($asocs);
+                    $A4 = count($ads);
                 }
                 else
                 {
-                    $tmpfin = $tmpfin * $numberpage;
+                    $A4 = $A4 * $ads_number;
                 }
-                for ($k=$tmpstrt*$numberpage;$k<$tmpfin;$k++)
+                for ($i = $A3 * $ads_number; $i < $A4; $i++)
                 {
-                    echo generate_dom($asocs[$k]);
+                    echo generate_dom($ads[$i]);
                 }
-
-                $tmpcp=intval(count($asocs)/$numberpage);
-                if(count($asocs)%$numberpage>0)
+                $A5 = intval(count($ads) / $ads_number);
+                if(count($ads) % $ads_number > 0)
                 {
-                    $tmpcp++;
+                    $A5++;
                 }
-                for($cp=0;$cp<$tmpcp;$cp++)
+                for($cp = 0; $cp < $A5; $cp++)
                 {
                     echo '<button name="Page" value="' . ($cp + 1) . '">' . ($cp + 1) . '</button>';
                 }
-                echo '<input type="number" name="LastSeen" value="' . $indpg . '"/>';
+                echo '<input type="number" name="LastSeen" value="' . $page_number . '"/>';
             }
             else
             {
@@ -242,40 +235,38 @@ if
             }
         }
     }
-
     function generate_dom($currentE1)
     {
         return
             '
-            <table style="background: ghostwhite" align="center" width="31%">
-                <tr>
-                    <td rowspan="5"><img align="right" width="150px" height="150px" alt="OLX_Img_1" src="' . $currentE1["URL"] . '"</td>
-                    <td style="background: lightgoldenrodyellow">' . $currentE1["Description"] . '</td>
-                </tr>
-                <tr>
-                    <td style="background: lightgoldenrodyellow">' . $currentE1["Cost"] . '</td>
+                <table style="background: ghostwhite" align="center" width="31%">
+                    <tr>
+                        <td rowspan="5"><img align="right" width="150px" height="150px" alt="OLX_Img_1" src="' . $currentE1["URL"] . '"</td>
+                        <td style="background: lightgoldenrodyellow">' . $currentE1["Description"] . '</td>
                     </tr>
-                <tr>
-                    <td style="background: lightgoldenrodyellow">' . $currentE1["Date"] . '</td>
-                </tr>
-                <tr>
-                    <td style="background: lightgoldenrodyellow">' . $currentE1["Adress"] . '</td>
-                </tr>
-                <tr>
-                    <td style="background: lightgoldenrodyellow">' . $currentE1["isTop"] . '</td>
-                </tr>
-            </table>
-        ';
+                    <tr>
+                        <td style="background: lightgoldenrodyellow">' . $currentE1["Cost"] . '</td>
+                        </tr>
+                    <tr>
+                        <td style="background: lightgoldenrodyellow">' . $currentE1["Date"] . '</td>
+                    </tr>
+                    <tr>
+                        <td style="background: lightgoldenrodyellow">' . $currentE1["Adress"] . '</td>
+                    </tr>
+                    <tr>
+                        <td style="background: lightgoldenrodyellow">' . $currentE1["isTop"] . '</td>
+                    </tr>
+                </table>
+            ';
     }
     if(isset($_GET['Page']))
     {
-        loadNext($_GET['Page'], $asoc);
+        loadNext($_GET['Page'], $ads_array);
     }
     else
     {
-        loadNext(1,$asoc);
+        loadNext(1, $ads_array);
     }
-
     ?>
 </form>
 </body>
